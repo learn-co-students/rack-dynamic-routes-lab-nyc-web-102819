6df5b50e7ec3b0a9b42877ee25eed1ddffcf639c
Barak Saidoff <barak.saidoff@gmail.com>
2019-11-18 14:46:20 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,8 @@ class Application
   
       if req.path.match(/items/)
         item_name = req.path.split("/items/").last
-        if item =@@items.find{|i| i.name == item_name}
+        # if item =@@items.find{|i| i.name == item_name}
+        if @@item.include?item_name
           resp.write item.price
         else 
           resp.status = 400
